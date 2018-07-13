c1f4f19b67ec5cee5bd5654ea3ea6cd6625975fc
maxwell <github email address>
2018-07-13 08:15:31 -0400

    added solution

--- src/Timer.js
+++ src/Timer.js
@@ -8,12 +8,13 @@ class Timer extends Component {
   }
 
   // add your code here
+  componentDidMount() {
+    this.interval = setInterval(this.clockTick, 1000)
+  }
 
-
-
-
-
-
+  componentWillUnmount() {
+    clearInterval(this.interval)
+  }
 
   render() {
 
